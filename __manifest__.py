ec1055ab3f790220f8d6326988e8823e82ea51b3
javiermt98 <javiermtdam@gmail.com>
2021-02-16 18:09:05 +0100

    v0.96 - Seguridad Falla Clientes

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        'security/estetica_security_view.xml',
         'security/ir.model.access.csv',
         'views/empleados_view.xml',
         'views/clientes_view.xml',
